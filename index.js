af249ff196dafd3577de5659ca909c571701e289
Damilola Adeleke <90906886+od-adeleke@users.noreply.github.com>
2022-06-23 20:57:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function add (num1, num2) {
+  const result = num1 + num2
+  console.log(result);
+}
+
+function subtract (num1, num2) {
+  const result = num1 - num2
+  console.log(result);
+}
+
+function divide (num1, num2) {
+  const result = num1 / num2
+  console.log(result);
+}
+
+function multiply (num1, num2) {
+  const result = num1 * num2
+  console.log(result);
+}
+
+add(2,3)
+subtract(7,5)
+divide(12,4)
+multiply(2,5)
